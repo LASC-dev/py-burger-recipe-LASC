38f78d5b2697376eac3a697d8345c2c1dcc44125
Lucas Adrian Sebastian Casas <lucas.a.s.casas@outlook.es>
2026-02-10 11:21:23 -0300

    feat: implemented Number class descriptor with validator logic.

--- app/main.py
+++ app/main.py
@@ -32,8 +32,26 @@ class Validator(ABC):
         pass
 
 
-class Number:
-    pass
+class Number(Validator):
+    def __init__(
+        self,
+        min_value: int,
+        max_value: int
+    ):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def validate(
+        self,
+        value: int
+    ):
+        if not isinstance(value, int):
+            raise TypeError("Quantity should be integer")
+        elif value < self.min_value and value > self.max_value:
+            raise ValueError(
+                f"Quantity should not be less than {self.min_value}",
+                f" and greater than {self.max_value}."
+            )
 
 
 class OneOf:
